29c2ce01173bd2b80923140bf6791546753b2fa8
Christina <nemovakg@gmail.com>
2023-10-11 18:18:26 +0300

    Задача 29

--- Program.cs
+++ Program.cs
@@ -23,19 +23,31 @@
 // 82 -> 10
 // 9012 -> 12
 
-System.Console.WriteLine("Введите число: ");
-string a = System.Console.ReadLine();
-int b = 0;
-if (Convert.ToDouble(a)%1 == 0)
-{
-    for (int i = 0; i < a.Length; i++)
-    {
-        b += Convert.ToInt32(a.Substring(i, 1));
-    }
-    System.Console.WriteLine($"Сумма всех цифр в числе = {b}");
-}
-else
-{
-    System.Console.WriteLine("Вы ввели не целое число");
-}
+// System.Console.WriteLine("Введите число: ");
+// string a = System.Console.ReadLine();
+// int b = 0;
+// if (Convert.ToDouble(a)%1 == 0)
+// {
+//     for (int i = 0; i < a.Length; i++)
+//     {
+//         b += Convert.ToInt32(a.Substring(i, 1));
+//     }
+//     System.Console.WriteLine($"Сумма всех цифр в числе = {b}");
+// }
+// else
+// {
+//     System.Console.WriteLine("Вы ввели не целое число");
+// }
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+System.Console.WriteLine("Эта программа создаст массив из 8 элементов и выведет его в терминал. Нажмите Enter");
+System.Console.ReadLine();
+int[] rndArr = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    rndArr[i] = new Random().Next(1,999);
+    Console.Write(rndArr[i] + " ");
+}
